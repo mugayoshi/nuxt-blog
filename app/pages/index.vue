afcd9026e5fd1110a6834be8b059c40e45624dff
mugayoshi <muga.y.j3039@gmail.com>
2020-05-02 23:30:24 +0900

    フロントエンド アカウント登録とログインの実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,7 +13,7 @@
           <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
         </div>
         <div class="text-right">
-          <el-button type="primary">{{ buttonText }}</el-button>
+          <el-button type="primary" @click="handleClickSubmit">{{ buttonText }}</el-button>
         </div>
       </form>
     </el-card>
@@ -21,9 +21,13 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 
 export default {
-  asyncData(){
+  asyncData({ redirect, store }){
+    if (store.getters['users']){
+      redirect('/posts')
+    }
     return {
       isCreateMode: false,
       formData: {
@@ -35,6 +39,55 @@ export default {
     buttonText() {
       return this.isCreateMode ? '新規登録' : 'ログイン'
     }
+  },
+  methods: {
+    async handleClickSubmit(){
+      if (this.isCreateMode) {
+        try {
+          console.log('trying to sign-up')
+          await this.register({...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'アカウント作成完了',
+            message: `${this.formData.id}として登録しました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+          console.log('an account is created!')
+          this.$router.push('/posts')
+        } catch(e){
+          this.$notify.error({
+            title: 'アカウント作成失敗',
+            message: '既に登録されているか，不正なユーザIDです',
+            position: 'bottom-right',
+            duration: 1000
+          })
+          
+        }
+      } else {
+        try {
+          console.log('trying to login')
+          await this.login({...this.formData})
+          this.$notify({
+             type: 'success',
+            title: 'ログイン成功',
+            message: `${this.formData.id}としてログインしました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+          console.log('loging success')
+          this.$router.push('/posts')
+        } catch (e) {
+          this.$notify.error({
+            title: 'ログイン作成失敗',
+            message: '不正なログインユーザIDです',
+            position: 'bottom-right',
+            duration: 1000
+          })
+        }
+      }
+    },
+    ...mapActions(['login', 'register'])
   }
 }
 </script>
---
+++ app/store/index.js
@@ -0,0 +1,34 @@
+export const store = () => ({
+    isLoggedIn: false,
+    user: null
+})
+
+export const getters = {
+    isLoggedIn: (state) => state.isLoggedIn,
+    user: (state) => state.user
+}
+
+export const mutations = {
+    setUser(state, { user }) {
+        state.user = user
+        state.isLoggedIn = true
+    }
+}
+
+export const actions = {
+    async login ({ commit }, { id }){
+        const user = await this.$axios.$get(`/users/${id}.json`)
+        // TODO: implement backend side
+        if (!user.id) throw new Error('Invalid user')
+        commit('serUser', { user })
+    },
+    async register({ commit }, { id }){
+        const payload = {}
+        payload[id] = { id }
+        await this.$axios.$patch(`/users.json`, payload)
+        const user = await this.$axios.$get(`/users/${id}.json`)
+        // TODO: implement backend side
+        if (!user.id) throw new Error('Invalid user')
+        commit('setUser', { user })
+    }
+}
